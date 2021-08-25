c054598e39eaf7eb8b3f41cd9a681a01fcb263fc
Ravi Ravitharan <ravi.ravitharan@wae.com>
2021-08-25 22:56:47 +0100

    balance parantheses: just ignore other charactors

--- Challenge_Ex/parentheses.py
+++ Challenge_Ex/parentheses.py
@@ -6,35 +6,34 @@ close_paran = [')','}', ']']
 
 def check_balance(string):
     stack = []
-    #check empty list
-    if not string:
-        print('Empty string')
-    else:
-        for x in string:
-            #open braces added to stack
-            if x in open_paran:
-                stack.append(x)
-            #close braces checked with last element of stack then pop out from stack
-            elif x in close_paran:
-                index = close_paran.index(x)
-                if(len(stack)>0 and (open_paran[index] == stack[len(stack)-1])):
-                    stack.pop()
-                else:
-                    break
+    for x in string:
+        #open braces added to stack
+        if x in open_paran:
+            stack.append(x)
+        #close braces checked with last element of stack then pop out from stack
+        elif x in close_paran:
+            index = close_paran.index(x)
+            if(len(stack)>0 and (open_paran[index] == stack[-1])):
+                stack.pop()
             else:
-                print('Invalid character is exist')
-                
-        #balance stack should be an empty list
-        if (len(stack) == 0):
-            print('balanced')
-        else:
-            print('unbalanced')
+                break
+    #balance stack should be an empty list
+    if stack:
+        return False
+    else:
+        return True
 
 
 #Input strings
-check_balance('{{)(}}')     #not balance
-check_balance('({)}')       #not balance
-check_balance('[({})]')     #balance  
-check_balance('{}([])')     #balance
-check_balance('{()}[[{}]]') #balance
-            
+strings = ['{{)(}}',    #unbalanced
+ '({a)}',                #unbalanced
+ '[a({})]',              #balanced
+ '{}([]a)',              #balanced
+ '{(a)}[[{}]]']          #balanced
+
+for string in strings:
+    print(f" {string:12}", end = '')
+    if check_balance(string):
+        print(" balanced")
+    else:
+        print(" unbalanced")
