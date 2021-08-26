859fb7511bbdf2bed8fe0096d60d50270320d273
Ravi Ravitharan <ravi.ravitharan@wae.com>
2021-08-26 19:04:39 +0100

    ISBN: clean up cosmetic

--- Challenge_Ex/isbn.py
+++ Challenge_Ex/isbn.py
@@ -1,72 +1,66 @@
 #check ISBN Number
 
-#print result
-def to_print(result):
-    if result:
-        print('Valid ISBN Number')
-    else:
-        print('Invalid ISBN Number')
-
-        
 #ISBN number validation
-def check_ISBN(num):
-    num = list(num)
+def check_isbn(isbn_code):
     #Remove sepiacl character
-    char = ['-',' ']
-
-    for x in num:
-        if x in char:
-            n = num.index(x)
-            del num[n]    
+    isbn_code = isbn_code.replace('-', '')
+    isbn_code = isbn_code.replace(' ', '')
 
     #check number count to define type
-    count = len(num)
+    count = len(isbn_code)
     tot = 0
     
     #For ISBN-13
     if (count==13):
         for i in range(count-1):
-            if ((i+1) % 2) == 0:
-                tot = tot + (int(num[i])*3)
+            if (i % 2) == 0:
+                tot = tot + int(isbn_code[i])
             else:
-                tot = tot + int(num[i])
+                tot = tot + (int(isbn_code[i])*3)
                 
         #get check digit             
         check_digit = (10-(tot%10))%10
         
         #check last digit
-        if check_digit == int(num[count-1]):
+        if check_digit == int(isbn_code[-1]):
             result = True
         else:
             result = False
             
     # For ISBN-10
     elif (count == 10):
-        for i in range(9):
-            tot = tot + int(num[i]) * (i+1)
+        for i in range(count-1):
+            tot = tot + int(isbn_code[i]) * (i+1)
                       
         #get check digit       
         check_digit = tot % 11
 
         #check last digit
-        if (num[count-1] == 'X'):
+        if (isbn_code[count-1] == 'X'):
             if(check_digit == 10):
                 result = True
             else:
                 result = False
-        elif check_digit == int(num[count-1]):
+        elif check_digit == int(isbn_code[-1]):
             result = True
         else:
             result = False
+    #Invalid ISBN
+    else:
+            result = False
 
-    return to_print(result)
+    return result
         
             
-
-
-    
 #Input ISBN Numbers
-check_ISBN('978 04700 59029')
-check_ISBN('0-321-14653-0')
-check_ISBN('1-23456-789-X')
-check_ISBN('0112112425')
+isbn_codes = [ '978 04700 59029',
+               '0-321-14653-0',
+               '1-23456-789-X',
+               '0112112425' ]
+
+for isbn in isbn_codes:
+    print(f"{isbn:16} is ", end = '')
+    if check_isbn(isbn):
+        print("Valid")
+    else:
+        print("Invalid")
