87d79e24523c3e48651f68443c473de52f4457fc
Kopiga Baskaran <bkopi95@gmail.com>
2021-09-02 00:00:40 +0530

    first commit

--- Challenge Ex/Ex_1_4.py
+++ Challenge Ex/Ex_1_4.py
@@ -1,28 +1,22 @@
-#Combined as large number
+#Combined a large number from list of numbers
+
+from itertools import permutations
 
 def combine_large(num):
-    num_list = []
-               
-    #convert to string
-    str_num = [str(x) for x in num]
-    print(str_num)
+    perm = permutations(num)
+    large_num = 0
 
-    for x in str_num:
-        for y in x:
-            
-            
-    #return print(''.join(str_num))
+    for x in perm:
+        num_list = map(str,x)
+        num = ''.join(list(num_list))
+        if int(num) > large_num:
+            large_num = int(num)
 
-
-#check non-negative numbers
-def check_num(num_list):
-    for x in num_list:
-        if x < 0:
-            print('Negative number is exist')
-            break
-    combine_large(num_list)
+    print(large_num)
+ 
+    
 
 combine_large([35, 5, 10, 9]) # 9+5+35+10
-#combine_large([50, 2, 1, 9])    #95021
-#check_num([5, 50, 56])      #56550     
-#check_num([420, 42, 423])   #42423420
+combine_large([50, 2, 1, 9])    #95021
+combine_large([5, 50, 56])      #56550     
+combine_large([420, 42, 423])   #42423420
